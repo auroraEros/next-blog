c6651a77fd4ba4ff5812d240065b0e667ea6c9f8
Sahar Javadi <sahar.javadi.h@gmail.com>
2025-05-13 18:34:16 +0330

    Add the create-post page interface and protect it as an Admin

--- src/app/_components/Header.jsx
+++ src/app/_components/Header.jsx
@@ -6,40 +6,36 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useTheme } from "next-themes";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-import { useEffect, useState } from "react";
 
 function Header() {
   const path = usePathname();
   const { theme, setTheme } = useTheme();
-
-
   return (
     <Navbar className="border-b-2">
-      {/* Logo */}
-      <Navbar.Brand as={Link} href="/">
+      <Link
+        href="/"
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+      >
         <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
           Sahar&apos;s
         </span>
-        <span className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white ml-2">
-          Blog
-        </span>
-      </Navbar.Brand>
-
+        Blog
+      </Link>
       {/* Search */}
-      <div className="flex md:order-2">
+      <form>
         <TextInput
           type="text"
           placeholder="Search..."
           rightIcon={AiOutlineSearch}
           className="hidden lg:inline"
         />
-        <Button className="w-12 h-10 lg:hidden" color="gray" pill>
-          <AiOutlineSearch />
-        </Button>
-      </div>
+      </form>
+      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
+        <AiOutlineSearch />
+      </Button>
 
       {/* Theme Toggle & Auth */}
-      <div className="flex items-center gap-2 md:order-3">
+      <div className="flex gap-2 md:order-2">
         <Button
           className="w-12 h-10 hidden sm:inline"
           color="gray"
@@ -68,15 +64,21 @@ function Header() {
 
       {/* Navigation Links */}
       <Navbar.Collapse>
-        <Navbar.Link href="/" active={path === "/"}>
-          Home
-        </Navbar.Link>
-        <Navbar.Link href="/about" active={path === "/about"}>
-          About
-        </Navbar.Link>
-        <Navbar.Link href="/projects" active={path === "/projects"}>
-          Projects
-        </Navbar.Link>
+        <Link href="/">
+          <Navbar.Link active={path === "/"} as={"div"}>
+            Home
+          </Navbar.Link>
+        </Link>
+        <Link href="/about">
+          <Navbar.Link active={path === "/about"} as={"div"}>
+            About
+          </Navbar.Link>
+        </Link>
+        <Link href="/projects">
+          <Navbar.Link active={path === "/projects"} as={"div"}>
+            Projects
+          </Navbar.Link>
+        </Link>
       </Navbar.Collapse>
     </Navbar>
   );
---
+++ src/app/dashboard/page.js
@@ -0,0 +1,7 @@
+
+
+function Page() {
+  return <div>post</div>
+}
+
+export default Page;
